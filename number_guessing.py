e4a7109116856788a3754150f74f7f1b489f3f65
praveenkumar <your_email@example.com>
2026-05-23 14:40:51 +0530

    editor edit

--- number_guessing.py
+++ number_guessing.py
@@ -14,11 +14,11 @@ for i in range(0,5):
         print("Your number is higher")
         
     elif user_input < secreat_number:
-<<<<<<< HEAD
+
         print("Your number is lesser")  
-=======
+
         print("Your number is lesser")
->>>>>>> ef56937fa45e707bc51d71827d69d2c313d22f89
+
         
     else:
         print("Your Guessing number is correct!!!")
